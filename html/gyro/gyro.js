8ff714c19bbc6da4e0c8149d6fc0ea08f9fd1bbc
robertdr1 <56233553+robertdr1@users.noreply.github.com>
2019-11-04 13:39:01 +0200

    gyro

---
+++ html/gyro/gyro.js
@@ -0,0 +1,10 @@
+document.getElementById(""id_logic).innerHTML = "Logic =2019.11.04.0";
+
+window.addEventListener("deviceorientation", on_gyro_data_uab);
+
+function on_gyro_data_uab(e)
+{
+	document.getElementById("id_alpha").innerHTML = e.alpha;
+	document.getElementById("id_beta").innerHTML = e.beta;
+	document.getElementById("id_gamma").innerHTML = e.gamma;
+}
