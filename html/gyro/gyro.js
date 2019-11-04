9d86f4ea363bd99d6b5fe2d0bbab9dfa3f3aa031
robertdr1 <56233553+robertdr1@users.noreply.github.com>
2019-11-04 15:20:30 +0200

    rotation

--- html/gyro/gyro.js
+++ html/gyro/gyro.js
@@ -17,4 +17,9 @@ function on_acc_data_uab(e)
 	document.getElementById("id_acc_y").innerHTML =Math.round(acc.y * 100) /100;
 	document.getElementById("id_acc_z").innerHTML =Math.round(acc.z * 100) /100;
 	
+	var rot_x = Math.atan(acc.x / acc.z) * 180 / Math.PI;
+	var rot_y = Math.atan(acc.y / acc.z) * 180 / Math.PI;
+	
+	document.getElementById("id_rot_x").innerHTML =Math.round(rot_x * 100) /100;
+	document.getElementById("id_rot_y").innerHTML =Math.round(rot_y * 100) /100;
 }
