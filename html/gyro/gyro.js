f789779da55d53f82bfa66f04867868f5ce1730b
robertdr1 <56233553+robertdr1@users.noreply.github.com>
2019-11-04 14:53:07 +0200

    fixed

--- html/gyro/gyro.js
+++ html/gyro/gyro.js
@@ -1,7 +1,7 @@
 document.getElementById("id_logic").innerHTML = "Logic =2019.11.04.0";
 
 window.addEventListener("deviceorientation", on_gyro_data_uab);
-window.addEventListener("deviceorientation", on_acc_data_uab);
+window.addEventListener("devicemotion", on_acc_data_uab);
 
 function on_gyro_data_uab(e)
 {
