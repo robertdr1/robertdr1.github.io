f6322f8ad8451a17484b2504cefb7bd5024b9b66
robertdr1 <56233553+robertdr1@users.noreply.github.com>
2019-11-25 12:43:29 +0200

    Add files via upload

---
+++ html/worker/prime.js
@@ -0,0 +1,12 @@
+
+function este_prim(n)
+{
+	for (var i = 2; i*i <= n; i ++)
+		if (n % i == 0)
+			return false;
+		return true;
+}
+
+for (var = 1E9; i < 1E11; i++)
+	if(este_prim(i))
+		postMessage(i);
