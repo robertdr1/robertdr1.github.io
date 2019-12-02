24172a224566c30e9c32b70059a170fa87e11954
robertdr1 <56233553+robertdr1@users.noreply.github.com>
2019-12-02 12:26:59 +0200

    Add files via upload

--- html/touch/touch.js
+++ html/touch/touch.js
@@ -1,4 +1,4 @@
-document.getElementById("id_logic").innerHTML = "Logic: 2019.12.02.3";
+document.getElementById("id_logic").innerHTML = "Logic: 2019.12.02.4";
 
 window.addEventListener("touchstart", touch_start_uab);
 window.addEventListener("touchmove", touch_move_uab);
@@ -6,6 +6,8 @@ window.addEventListener("touchend", touch_end_uab);
 
 var canvas = document.getElementById("id_canvas");
 var context = canvas.getContext("2d");
+
+var canvas_rect = canvas.getBoundingClientRect();
 
 var last_position = [];
 
@@ -24,7 +26,7 @@ function touch_start_uab(p)
 	for (var i = 0; i < p.changedTouches.length; i++)
 	{
 		context.beginPath();
-		context.arc(t[i].pageX, t[i].pageY, 10, 0 ,2 * Math.PI);
+		context.arc(t[i].pageX - canvas_rect.left, t[i].pageY - cavas_rect.top, 10, 0 ,2 * Math.PI);
 		context.stroke();
 		
 		var touch_info ={};
@@ -58,12 +60,15 @@ function touch_move_uab(p)
 			}
 		context.beginPath();
 		context.moveTo(last_position[index_t].x, last_position[index_t].y);
-		context.lineTo(t[i].pageX, t[i].pageY);
+		context.lineTo(t[i].pageX-canvas_rect.left, t[i].pageY-canvas_rect.top);
 		context.strokeStyle = touch_info.color;
 		context.fillStyle = touch_info.color;
 		context.lineWidth = 20;
 		context.fill();
 		context.stroke();
+		
+		last_position[index_t].x = t[i]pageX;
+		last_position[index_t].y = t[i].pageY;
 	}
 }
 
